cce1d5948bff255a6f29be2f1e7ad5f45673b53b
Xavier Soria Poma <xsoriap@yahoo.com>
2022-08-20 06:37:34 -0500

    V10-10 UNSnet BN+AF just Xav init normal

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ def parse_args():
                         help='use previous trained data')  # Just for test
     parser.add_argument('--checkpoint_data',
                         type=str,
-                        default='9/9_model.pth',# 37 for biped 60 MDBD
+                        default='14/14_model.pth',# 37 for biped 60 MDBD
                         help='Checkpoint path.')
     parser.add_argument('--test_img_width',
                         type=int,
