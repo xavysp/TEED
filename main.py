18db5c120461909a564e09293b54f469d4f69cf1
Xavier Soria Poma <xsoriap@yahoo.com>
2022-09-10 11:02:12 -0500

    v13-2 BIPEDext

--- main.py
+++ main.py
@@ -281,7 +281,7 @@ def parse_args():
                         help='use previous trained data')  # Just for test
     parser.add_argument('--checkpoint_data',
                         type=str,
-                        default='12/12_model.pth',# 37 for biped 60 MDBD
+                        default='4/4_model.pth',# 37 for biped 60 MDBD
                         help='Checkpoint path.')
     parser.add_argument('--test_img_width',
                         type=int,
@@ -305,16 +305,16 @@ def parse_args():
                         default=15,
                         metavar='N',
                         help='Number of training epochs (default: 25).')
-    parser.add_argument('--lr', default=3e-4, type=float,
+    parser.add_argument('--lr', default=5e-5, type=float,
                         help='Initial learning rate. =5e-5')
-    parser.add_argument('--lrs', default=[6e-7,1e-4,3e-4], type=float,
+    parser.add_argument('--lrs', default=[25e-4,5e-4,3e-4], type=float,
                         help='LR for epochs')
-    parser.add_argument('--wd', type=float, default=1e-7, metavar='WD',
-                        help='weight decay (Good 1e-5 LDC 0.)') # Test left= WD 5e-5
+    parser.add_argument('--wd', type=float, default=0.0, metavar='WD',
+                        help='weight decay (Good 1e-7 LDC 0.)') # Test left= WD 5e-5
     parser.add_argument('--adjust_lr', default=[2,4,6], type=int,
                         help='Learning rate step size.')  # [6,9,19]
     parser.add_argument('--version_notes',
-                        default=' V12-9X00 TDC-BIPEDext AF=Smish -USNet AF  Just xav init normal BDCNloss2+CatsLoss2 CofusionWDCNOsmish+(return Fmish()) NewImean',
+                        default=' V13-2 TDC-BIPEDext AF=Smish -USNet AF  Just xav init normal BDCNloss2+CatsLoss2 CofusionWDCNOsmish+(return Fmish()) NewImean',
                         type=str,
                         help='version notes')
     parser.add_argument('--batch_size',
