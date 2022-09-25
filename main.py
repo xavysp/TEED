7b8421aad44d2ed88681ae65b75bd84bb2ceeb28
Xavier Soria Poma <xsoriap@yahoo.com>
2022-09-24 23:33:23 -0500

    v5-5 BIPBRI  augB5*

--- main.py
+++ main.py
@@ -282,7 +282,7 @@ def parse_args():
                         help='use previous trained data')  # Just for test
     parser.add_argument('--checkpoint_data',
                         type=str,
-                        default='11/11_model.pth',# 37 for biped 60 MDBD
+                        default='10/10_model.pth',# 37 for biped 60 MDBD
                         help='Checkpoint path.')
     parser.add_argument('--test_img_width',
                         type=int,
@@ -308,7 +308,7 @@ def parse_args():
                         help='Number of training epochs (default: 25).')
     parser.add_argument('--lr', default=6e-5, type=float,
                         help='Initial learning rate. =5e-5')
-    parser.add_argument('--lrs', default=[3e-5], type=float,
+    parser.add_argument('--lrs', default=[4e-5], type=float,
                         help='LR for epochs') #  [0.0007, 5e-05, 1e-05]
     parser.add_argument('--wd', type=float, default=5e-6, metavar='WD',
                         help='weight decay (Good 1e-5 and  5e-6)') # Test left= WD 5e-5
