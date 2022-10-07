6afbee8407b2d97b3ce28a6a3f6f16e07b0a47da
Xavier Soria Poma <xsoriap@yahoo.com>
2022-10-06 22:48:32 -0500

    v5-9 BIPED  aug0*

--- main.py
+++ main.py
@@ -317,7 +317,7 @@ def parse_args():
     parser.add_argument('--adjust_lr', default=[4], type=int,
                         help='Learning rate step size.')  # [6,9,19]
     parser.add_argument('--version_notes',
-                        default=' V5-9 TDC-BIPED augB0 AF=Smish -USNet---noBN  Just xav init normal BDCNloss2+CatsLoss2 CofusionWDCNOsmish+sum not multi(return Fmish())',
+                        default=' V5-9 TDC-BIPED augB0 AF=Smish -USNet---noBN  Just xav init normal BDCNloss2+CatsLoss2 CofusionWDCNOsmish+mean(return Fmish())',
                         type=str,
                         help='version notes')
     parser.add_argument('--batch_size',
