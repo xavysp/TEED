cc66cb2314925df14eee49b879d0af902b1443ea
Xavier Soria Poma <xsoriap@yahoo.com>
2022-10-09 22:40:13 -0500

    v5-9 BIPED  aug0*

--- modelSmishArch.py
+++ modelSmishArch.py
@@ -78,8 +78,8 @@ class CoFusionDWC(nn.Module):
         # attn = self.PSconv1(self.DWconv1(self.smish(x))) # [8, 32, 352, 352] self.smish(
         attn = self.PSconv1(self.DWconv1(x)) # [8, 32, 352, 352] self.smish(
         # attn = self.relu(self.norm_layer2(self.conv2(attn)))
-        # attn2 = self.PSconv1(self.DWconv2(attn)) # self.smish( self.relu( commented for evaluation [8, 3, 352, 352]
-        attn2 = self.PSconv1(self.DWconv2(self.smish(attn))) # self.smish( self.relu( commented for evaluation [8, 3, 352, 352]
+        attn2 = self.PSconv1(self.DWconv2(attn)) # self.smish( self.relu( commented for evaluation [8, 3, 352, 352]
+        # attn2 = self.PSconv1(self.DWconv2(self.smish(attn))) # self.smish( self.relu( commented for evaluation [8, 3, 352, 352]
 
         # return ((fusecat * attn).sum(1)).unsqueeze(1)
         # return Fmish(((attn2 * attn).sum(1)).unsqueeze(1)) #Fsmish Ori mine
