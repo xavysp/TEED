1c2666771f89364e9d5e3f9b5a034ada8efbe90f
Xavier Soria Poma <xsoriap@yahoo.com>
2022-10-16 12:40:51 -0500

    v5-9 BIPED  aug0*

--- main.py
+++ main.py
@@ -312,7 +312,7 @@ def parse_args():
                         help='Initial learning rate. =5e-5')
     parser.add_argument('--lrs', default=[5e-5], type=float,
                         help='LR for epochs') #  [0.0007, 5e-05, 1e-05]
-    parser.add_argument('--wd', type=float, default=1e-6, metavar='WD',
+    parser.add_argument('--wd', type=float, default=1e-7, metavar='WD',
                         help='weight decay (Good 1e-5 and  5e-6)') # Test left= WD 5e-5
     parser.add_argument('--adjust_lr', default=[4], type=int,
                         help='Learning rate step size.')  # [6,9,19]
