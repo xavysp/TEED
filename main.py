40d9daf255830c03ef9808e8579be66f1d12338d
Xavier Soria Poma <xsoriap@yahoo.com>
2022-10-24 23:37:06 -0500

    v5-9 BIPED  aug0*

--- main.py
+++ main.py
@@ -309,13 +309,13 @@ def parse_args():
                         default=15,
                         metavar='N',
                         help='Number of training epochs (default: 25).')
-    parser.add_argument('--lr', default=5e-5, type=float,
+    parser.add_argument('--lr', default=8e-4, type=float,
                         help='Initial learning rate. =5e-5')
-    parser.add_argument('--lrs', default=None, type=float,
+    parser.add_argument('--lrs', default=[8e-5], type=float,
                         help='LR for epochs') #  [5e-5] [0.0007, 5e-05, 1e-05]
     parser.add_argument('--wd', type=float, default=1e-6, metavar='WD',
                         help='weight decay (Good 1e-5  1e-7 and  5e-6)') # Test left= WD 5e-5
-    parser.add_argument('--adjust_lr', default=None, type=int,
+    parser.add_argument('--adjust_lr', default=[4], type=int,
                         help='Learning rate step size.')  # [4] [6,9,19]
     parser.add_argument('--version_notes',
                         default=' V5-9 TED-BIPED augB0 AF=Smish -USNet---noBN  Just xav init normal bdcnLoss2 +CofusionWDCN-sum +1smish',
