730222bbe6c0cf06aeec6fce33b127bb616fb2f3
Xavier Soria Poma <xsoriap@yahoo.com>
2022-10-27 13:06:41 -0500

    v5-9 BIPED  aug0*

--- main.py
+++ main.py
@@ -313,12 +313,12 @@ def parse_args():
                         help='Initial learning rate. =5e-5')
     parser.add_argument('--lrs', default=[8e-5], type=float,
                         help='LR for epochs') #  [5e-5] [0.0007, 5e-05, 1e-05]
-    parser.add_argument('--wd', type=float, default=1e-5, metavar='WD',
+    parser.add_argument('--wd', type=float, default=5e-5, metavar='WD',
                         help='weight decay (Good 1e-4  1e-5 and  5e-6)') # Test left= WD 5e-5
     parser.add_argument('--adjust_lr', default=[4], type=int,
                         help='Learning rate step size.')  # [4] [6,9,19]
     parser.add_argument('--version_notes',
-                        default=' V15 TED-BIPED augB0 AF=Smish -USNet---noBN  Just xav init normal bdcnLoss2+cats2loss +CofusionLDC',
+                        default=' V16 TED-BIPED augB0 AF=Smish -USNet---noBN  Just xav init normal bdcnLoss2+cats2loss +CofusionLDC',
                         type=str,
                         help='version notes')
     parser.add_argument('--batch_size',
