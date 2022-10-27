57f0098f8ceb8dbe343fc1dcf2cf5fd849bbd87d
Xavier Soria Poma <xsoriap@yahoo.com>
2022-10-27 17:30:04 -0500

    v5-9 BIPED  aug0*

--- main.py
+++ main.py
@@ -285,7 +285,7 @@ def parse_args():
                         help='use previous trained data')  # Just for test
     parser.add_argument('--checkpoint_data',
                         type=str,
-                        default='7/7_model.pth',# 37 for biped 60 MDBD
+                        default='14/14_model.pth',# 37 for biped 60 MDBD
                         help='Checkpoint path.')
     parser.add_argument('--test_img_width',
                         type=int,
@@ -311,14 +311,14 @@ def parse_args():
                         help='Number of training epochs (default: 25).')
     parser.add_argument('--lr', default=1e-3, type=float,
                         help='Initial learning rate. =5e-5')
-    parser.add_argument('--lrs', default=[8e-5], type=float,
+    parser.add_argument('--lrs', default=[7e-5], type=float,
                         help='LR for epochs') #  [5e-5] [0.0007, 5e-05, 1e-05]
-    parser.add_argument('--wd', type=float, default=5e-5, metavar='WD',
+    parser.add_argument('--wd', type=float, default=1e-4, metavar='WD',
                         help='weight decay (Good 1e-4  1e-5 and  5e-6)') # Test left= WD 5e-5
     parser.add_argument('--adjust_lr', default=[4], type=int,
                         help='Learning rate step size.')  # [4] [6,9,19]
     parser.add_argument('--version_notes',
-                        default=' V16 TED-BIPED augB0 AF=Smish -USNet---noBN  Just xav init normal bdcnLoss2+cats2loss +CofusionLDC',
+                        default=' V14-1 TED-BIPED augB0 AF=Smish -USNet---noBN  Just xav init normal bdcnLoss2+cats2loss +CofusionLDC',
                         type=str,
                         help='version notes')
     parser.add_argument('--batch_size',
