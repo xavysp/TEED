89afd755f628c2baabc39bc2df1911de6f2dffdc
Xavier Soria Poma <xsoriap@yahoo.com>
2022-11-09 19:48:00 -0500

    V14-6-320-5smis BIPED augB3

--- dataset.py
+++ dataset.py
@@ -123,8 +123,8 @@ def dataset_info(dataset_name, is_linux=True):
                 'img_width': 1280, # 1280 5 1920
                 'test_list': 'test_pair.lst',
                 # 'train_list': 'train_pair0.lst', # Base augmentation
-                # 'train_list': 'train_pairB3.lst', # another augmentation
-                'train_list': 'train_pairB5.lst', # Last augmentation
+                'train_list': 'train_pairB3.lst', # another augmentation
+                # 'train_list': 'train_pairB5.lst', # Last augmentation
                 'data_dir': '/root/workspace/datasets/BIPED',  # mean_rgb
                 'yita': 0.5,
                 'mean':BIPED_mean
