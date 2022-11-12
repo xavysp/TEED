624e63bcedbcf83a6c0cfa46611bd4d9f09f2419
xavysp <xsoriap@yahoo.com>
2022-11-11 20:23:26 -0500

    V14-l1-300 smis BIPED aug0

--- ted.py
+++ ted.py
@@ -94,12 +94,12 @@ class CoFusionDWC(nn.Module):
 
     def forward(self, x):
         # fusecat = torch.cat(x, dim=1)
-        attn = self.PSconv1(self.DWconv1(self.AF(x))) # [8, 32, 352, 352] self.smish(
-        # attn = self.PSconv1(self.DWconv1(x)) #self.smish( BIPBRI
+        # attn = self.PSconv1(self.DWconv1(self.AF(x))) # [8, 32, 352, 352] self.smish(
+        attn = self.PSconv1(self.DWconv1(x)) #self.smish( BIPBRI
         # attn = self.AF(self.PSconv1(self.DWconv1(x))) # v14-5-352
 
-        attn2 = self.PSconv1(self.DWconv2(self.AF(attn))) # self.smish( self.relu( commented for evaluation [8, 3, 352, 352]
-        # attn2 = self.PSconv1(self.DWconv2(attn)) # self.smish( self.relu(  4BIPBRI
+        # attn2 = self.PSconv1(self.DWconv2(self.AF(attn))) # self.smish( self.relu( commented for evaluation [8, 3, 352, 352]
+        attn2 = self.PSconv1(self.DWconv2(attn)) # self.smish( self.relu(  4BIPBRI
         # attn2 = self.AF(self.PSconv1(self.DWconv2(attn))) # self.smish( self.relu( # v14-5-352
 
         # return ((fusecat * attn).sum(1)).unsqueeze(1) # ori
