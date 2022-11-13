0d55db2a868df75eeb33b59ff57005652c4e0548
xavysp <xsoriap@yahoo.com>
2022-11-13 12:05:56 -0500

    V14-l1-300 smis BIPED aug0

--- tedXaf.py
+++ tedXaf.py
@@ -126,8 +126,8 @@ class _DenseLayer(nn.Sequential):
     def forward(self, x):
         x1, x2 = x
 
-        new_features = super(_DenseLayer, self).forward(Fsmish(x1))  # F.relu() ORI
-        # new_features = super(_DenseLayer, self).forward(x1)  # F.relu()
+        # new_features = super(_DenseLayer, self).forward(Fsmish(x1))  # F.relu() ORI
+        new_features = super(_DenseLayer, self).forward(x1)  # F.relu()
         # if new_features.shape[-1]!=x2.shape[-1]:
         #     new_features =F.interpolate(new_features,size=(x2.shape[2],x2.shape[-1]), mode='bicubic',
         #                                 align_corners=False)
