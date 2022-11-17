392bdbe23b93e7756014b4d2b213c6272a9aabaa
xavysp <xsoriap@yahoo.com>
2022-11-16 23:09:32 -0500

    V14-l1-300 silu BIPED aug0

--- main.py
+++ main.py
@@ -316,7 +316,7 @@ def parse_args():
     parser.add_argument('--adjust_lr', default=[4], type=int,
                         help='Learning rate step size.')  # [4] [6,9,19]
     parser.add_argument('--version_notes',
-                        default='V14-l1-300-4 aug0 BIRND+BIPED-trainingdataLoaderSetting AF=tanh+smish+Xaf -USNet---noBN xav init normal bdcnLoss2+cats2loss +DoubleF-DWC-2 before1After AF sum',
+                        default='V14-l1-300-4 aug0 BIRND+BIPED-trainingdataLoaderSetting AF=tanh+smish+Xaf -USNet---noBN xav init normal bdcnLoss2+cats2loss +DoubleF-DWC-2After AF sum',
                         type=str,
                         help='version notes')
     parser.add_argument('--batch_size',
