68a37dee104cef07a19d2f694b42bc1e5dcfe324
xavysp <xsoriap@yahoo.com>
2022-11-16 23:08:38 -0500

    V14-l1-300 silu BIPED aug0

--- tedXaf.py
+++ tedXaf.py
@@ -15,7 +15,7 @@ from utils.AF.Xsmish import Smish
 from utils.AF.Fxaf import xaf as Fxaf
 from utils.AF.Xxaf import Xaf
 
-AF = nn.SiLU # nn.Tanh # #nn.ReLU(inplace=True)
+AF = nn.Tanh # nn.Tanh # #nn.ReLU(inplace=True)
 
 def weight_init(m):
     if isinstance(m, (nn.Conv2d,)):
@@ -95,12 +95,12 @@ class CoFusionDWC(nn.Module):
 
     def forward(self, x):
         # fusecat = torch.cat(x, dim=1)
-        attn = self.PSconv1(self.DWconv1(self.af(x))) # [8, 32, 352, 352] self.smish(
+        # attn = self.PSconv1(self.DWconv1(self.af(x))) # [8, 32, 352, 352] self.smish(
         # attn = self.PSconv1(self.DWconv1(x)) #self.smish( BIPBRI
-        # attn = self.af(self.PSconv1(self.DWconv1(x))) # v14-5-352
+        attn = self.af(self.PSconv1(self.DWconv1(x))) # v14-5-352
 
-        attn2 = self.PSconv1(self.DWconv2(self.af(attn))) # self.smish( self.relu( commented for evaluation [8, 3, 352, 352]
-        # attn2 = self.PSconv1(self.DWconv2(attn)) # self.smish( self.relu(  4BIPBRI
+        # attn2 = self.PSconv1(self.DWconv2(self.af(attn))) # self.smish( self.relu( commented for evaluation [8, 3, 352, 352]
+        attn2 = self.PSconv1(self.DWconv2(attn)) # self.smish( self.relu(  4BIPBRI
         # attn2 = self.af(self.PSconv1(self.DWconv2(attn))) # self.smish( self.relu( # v14-5-352
 
         # return ((fusecat * attn).sum(1)).unsqueeze(1) # ori
@@ -126,7 +126,7 @@ class _DenseLayer(nn.Sequential):
     def forward(self, x):
         x1, x2 = x
 
-        new_features = super(_DenseLayer, self).forward(F.selu(x1))  # F.relu() ORI
+        new_features = super(_DenseLayer, self).forward(F.tanh(x1))  # F.relu() ORI
         # new_features = super(_DenseLayer, self).forward(x1)  # F.relu()
         # if new_features.shape[-1]!=x2.shape[-1]:
         #     new_features =F.interpolate(new_features,size=(x2.shape[2],x2.shape[-1]), mode='bicubic',
