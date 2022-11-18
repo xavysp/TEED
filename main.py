bc8391d9b1026ea2ea365388c3d7af7d713b6cd8
xavysp <xsoriap@yahoo.com>
2022-11-17 21:24:54 -0500

    V14-l1-300 silu BIPED aug0

--- main.py
+++ main.py
@@ -232,7 +232,7 @@ def parse_args():
     TEST_DATA = DATASET_NAMES[parser.parse_args().choose_test_data] # max 8
     test_inf = dataset_info(TEST_DATA, is_linux=IS_LINUX)
 
-    is_testing =False
+    is_testing =True
     # Training settings
     # BIPED-B2=1, BIPDE-B3=2, just for evaluation, using LDC trained with 2 or 3 bloacks
     TRAIN_DATA = DATASET_NAMES[0] # BIPED=0, BRIND=6, MDBD=10, BIPBRI=13
@@ -309,7 +309,7 @@ def parse_args():
                         help='Number of training epochs (default: 25).')
     parser.add_argument('--lr', default=1e-3, type=float,
                         help='Initial learning rate. =1e-3') # 1e-3
-    parser.add_argument('--lrs', default=[8e-5], type=float,
+    parser.add_argument('--lrs', default=[7e-5], type=float,
                         help='LR for epochs') #  [7e-5]
     parser.add_argument('--wd', type=float, default=1e-4, metavar='WD',
                         help='weight decay (Good 5e-4  )') # Test left= WD 5e-5
