6528caf05eb137a8e0bc2e653a60a85a0b631cdc
xavysp <xsoriap@yahoo.com>
2022-11-20 10:26:14 -0500

    V14-l1-300-5 TED BIPED aug0

--- main.py
+++ main.py
@@ -283,7 +283,7 @@ def parse_args():
                         help='use previous trained data')  # Just for test
     parser.add_argument('--checkpoint_data',
                         type=str,
-                        default='4/4_model.pth',# 37 for biped 60 MDBD
+                        default='7/7_model.pth',# 37 for biped 60 MDBD
                         help='Checkpoint path.')
     parser.add_argument('--test_img_width',
                         type=int,
@@ -316,7 +316,7 @@ def parse_args():
     parser.add_argument('--adjust_lr', default=[4], type=int,
                         help='Learning rate step size.')  # [4] [6,9,19]
     parser.add_argument('--version_notes',
-                        default='V14-l1-5 aug0 BIPED+BRIND-trainingdataLoader AF=smish+Xaf -USNet---noBN xav init normal bdcnLoss2+cats2loss +DoubleF-DWC-3smish AF sum',
+                        default='V14-l1-5 aug0 BIPED+BRIND-trainingdataLoader AF=smish -USNet---noBN xav init normal bdcnLoss2+cats2loss +DoubleF-DWC-3smish AF sum',
                         type=str,
                         help='version notes')
     parser.add_argument('--batch_size',
