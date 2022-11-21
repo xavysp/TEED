54839f1cfc571bb4fa86f4f87ff0a6cbddafcbe7
xavysp <xsoriap@yahoo.com>
2022-11-21 01:02:30 -0500

    V14-l1-300-5 TED BIPED aug0

--- tedXaf.py
+++ tedXaf.py
@@ -15,7 +15,7 @@ from utils.AF.Xsmish import Smish
 from utils.AF.Fxaf import xaf as Fxaf
 from utils.AF.Xxaf import Xaf
 
-AF = Smish()#nn.Tanh # nn.Tanh # #nn.ReLU(inplace=True)
+AF = Smish#nn.Tanh # nn.Tanh # #nn.ReLU(inplace=True)
 
 def weight_init(m):
     if isinstance(m, (nn.Conv2d,)):
