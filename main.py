b97dedf9b885f7a1a41dc5981af55aa86b54ea64
xavysp <xsoriap@yahoo.com>
2023-01-06 16:40:35 -0500

    V14-l1-5 TED in tedXaf BIPED aug0

--- main.py
+++ main.py
@@ -323,7 +323,7 @@ def parse_args():
     parser.add_argument('--adjust_lr', default=[4], type=int,
                         help='Learning rate step size.')  # [4] [6,9,19]
     parser.add_argument('--version_notes',
-                        default='V14-l1-5 TED BIPED+BRIND-trainingdataLoader AF=Relu -USNet---noBN xav init normal bdcnLoss2+cats2loss +DoubleF-DWC-3Smish AF sum',
+                        default='V14-l1-5 TEDxaf BIPED+BRIND-trainingdataLoader AF=Relu -USNet---noBN xav init normal bdcnLoss2+cats2loss +DoubleF-DWC-3Smish AF sum',
                         type=str,
                         help='version notes')
     parser.add_argument('--batch_size',
