a60ba0b3884d5b67a02ce0550df6cdd26cabb9db
xavysp <xsoriap@yahoo.com>
2023-01-07 08:37:04 -0500

    V14-l1-5 TED in tedXaf Xaf BIPED aug0

--- utils/AF/Fxaf.py
+++ utils/AF/Fxaf.py
@@ -17,8 +17,8 @@ def xaf(input):
     # x = torch.log(0.25+(1/( 0.25+torch.exp(-input))))# ori good
     # x =(-1+(1/(torch.exp(-input)))) # sigmoid modefied
     # np.clip(fx+0.25,-0.5,0.9)
-    x = torch.clip(x+0.2,-0.5,0.9) # this help ti thin the edge check 2
+    # x = torch.clip(x+0.2,-0.5,0.9) # this help ti thin the edge check 2
     # adding 0.25
     # x = torch.clip(torch.tanh(x)+0.25,-0.5,0.9)
-    # x = torch.clip(x,-0.5,0.9)
+    x = torch.clip(x,-0.5,0.9)
     return input * x
