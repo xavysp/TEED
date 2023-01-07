87e2140fc777ee93ca530e3a18af35cf42f33953
xavysp <xsoriap@yahoo.com>
2023-01-07 08:34:30 -0500

    V14-l1-5 TED in tedXaf Xaf BIPED aug0

--- utils/AF/Fxaf.py
+++ utils/AF/Fxaf.py
@@ -12,9 +12,9 @@ def xaf(input):
     mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(sigmoid(x))))
     See additional documentation for mish class.
     """
-    # x = torch.tanh(-1+(1/(0.1 + torch.exp(-input))))
+    x = torch.tanh(-1+(1/(0.1 + torch.exp(-input))))
     # x = torch.log(1+torch.sigmoid(input))# ori good
-    x = torch.log(0.25+(1/( 0.25+torch.exp(-input))))# ori good
+    # x = torch.log(0.25+(1/( 0.25+torch.exp(-input))))# ori good
     # x =(-1+(1/(torch.exp(-input)))) # sigmoid modefied
     # np.clip(fx+0.25,-0.5,0.9)
     x = torch.clip(x+0.2,-0.5,0.9) # this help ti thin the edge check 2
