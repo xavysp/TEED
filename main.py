6a4fe37ae1c403c6e3865963265b65e962c562d7
xavysp <xsoriap@yahoo.com>
2023-01-10 07:02:41 -0500

    V14-l1-3v8 TED smish BIPED aug0

--- main.py
+++ main.py
@@ -289,7 +289,7 @@ def parse_args():
                         help='use previous trained data')  # Just for test
     parser.add_argument('--checkpoint_data',
                         type=str,
-                        default='4/4_model.pth',# 37 for biped 60 MDBD
+                        default='3/3_model.pth',# 37 for biped 60 MDBD
                         help='Checkpoint path.')
     parser.add_argument('--test_img_width',
                         type=int,
