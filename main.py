f3165087a3087a425019dfcea1e17615c8a9e5f6
xavysp <xsoriap@yahoo.com>
2023-01-11 16:13:05 -0500

    V14-l1-3v7 TED smish BIPED aug0

--- main.py
+++ main.py
@@ -224,7 +224,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='LDC trainer.')
     parser.add_argument('--choose_test_data',
                         type=int,
-                        default=-1,
+                        default=-14,
                         help='Choose a dataset for testing: 0 - 15')
     # UDED=14
     # ----------- test -------0--
