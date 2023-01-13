2bf972d4b8d799c658b9e2cbc90a35414f0a41ef
xavysp <xsoriap@yahoo.com>
2023-01-13 08:46:45 -0500

    V14-l1-3v73 TED smish BIPED aug0

--- main.py
+++ main.py
@@ -107,8 +107,9 @@ def train_one_epoch(epoch, dataloader, model, criterions, optimizer, device,
                                    img_test,
                                    (x, y),
                                    font, font_size, font_color, font_thickness, cv2.LINE_AA)
-            # tmp_vis_name = str(batch_id)+'-results.png'
-            cv2.imwrite(os.path.join(imgs_res_folder, 'results.png'), vis_imgs)
+            tmp_vis_name = str(batch_id)+'-results.png'
+            cv2.imwrite(os.path.join(imgs_res_folder, tmp_vis_name), vis_imgs)
+            # cv2.imwrite(os.path.join(imgs_res_folder, 'results.png'), vis_imgs)
     loss_avg = np.array(loss_avg).mean()
     return loss_avg
 
