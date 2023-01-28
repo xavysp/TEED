460fb760ccb150c6ebf011e8e9e5d54eb28064e6
xavysp <xsoriap@yahoo.com>
2023-01-28 15:05:47 -0500

    Cleaner TEED repo

--- main.py
+++ main.py
@@ -312,7 +312,7 @@ def parse_args():
     parser.add_argument('--adjust_lr', default=[4], type=int,
                         help='Learning rate step size.')  # [4] [6,9,19]
     parser.add_argument('--version_notes',
-                        default='TED BIPED+BRIND-trainingdataLoader AF=smish -USNet--noBN xav init normal bdcnLoss2+cats2loss +DoubleFusio-3Smish AF sum',
+                        default='TED BIPED+BRIND-trainingdataLoader AF=smish -USNet--noBN xav init normal bdcnLoss2+cats2loss +DoubleFusion-3Smish AF sum',
                         type=str,
                         help='version notes')
     parser.add_argument('--batch_size',
